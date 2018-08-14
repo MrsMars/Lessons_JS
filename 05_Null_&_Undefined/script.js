eae54a3a266d647085da6d9dd8ae99dc8238b608
Allega <mr.oger@mail.ru>
2018-08-14 16:43:15 +0300

    some changes with 'null and undefined'

---
+++ 05_Null_&_Undefined/script.js
@@ -0,0 +1,17 @@
+// null - is an empty value
+// undefined - is a value absence
+
+var temp,
+    obj = {},
+    a = [1, 2, 3, 4];
+
+function greet(name){
+    return "Hello " + name;
+}
+
+console.log(temp);              // undefined
+console.log(obj.property);      // undefined
+console.log(a[4]);              // undefined
+
+console.log(greet("Allo"));     // Hello Allo
+console.log(greet());           // Hello undefined
