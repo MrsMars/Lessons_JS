b4fc9c10fe4bf8d0653edab6b620feaceaa344dc
Allega <42183540+MrsMars@users.noreply.github.com>
2018-10-20 17:12:21 +0300

    html() & text()

---
+++ jQuery.Lesson_11.Functions_html()_and_text()/script.js
@@ -0,0 +1,14 @@
+console.log($("#div").html());      /*
+                                        Block
+                                        <p data-brackets-id="108">
+                                            Some article
+                                        </p>
+                                    */
+
+console.log($("#div").text());      /*
+                                        Block
+                                        
+                                            Some article
+                                    */
+
+$("p").html("<p>New article</p>");
